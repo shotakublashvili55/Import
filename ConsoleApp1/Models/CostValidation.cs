680aa70bcc535c9b6e6b0dfdffe097fad351a6f5
shotakublashvili55 <shotakublashvili55@gmail.com>
2025-02-01 17:36:53 +0400

    add_validations

--- ConsoleApp1/Models/CostValidation.cs
+++ ConsoleApp1/Models/CostValidation.cs
@@ -7,6 +7,21 @@ internal class CostValidation: AbstractValidator<Cost>
     public CostValidation() 
     {
       RuleFor(cost => cost.CostSum)
-            .GreaterThan(0).WithMessage("Ammount must greater then 0"); ;
+            .GreaterThan(0).WithMessage("Ammount must greater then 0");
+
+        RuleFor(cost => cost.PrepaymentCurrency1)
+              .NotEmpty()
+              .When(cost => cost.PrepaymentSum1 > 0).WithMessage("Invalid Currency Name");
+
+        RuleFor(cost => cost.PrepaymentCurrency2)
+      .NotEmpty()
+      .When(cost => cost.PrepaymentSum2 > 0).WithMessage("Invalid Currency Name");
+
+        RuleFor(cost => cost.PrepaymentCurrency3)
+      .NotEmpty()
+      .When(cost => cost.PrepaymentSum3 > 0).WithMessage("Invalid Currency Name");
+
     }
+
+   
 }
